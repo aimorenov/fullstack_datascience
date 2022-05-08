5620c68867c0fd3827547574714dd1ccb185075d
aimorenov <ai.morenovega@gmail.com>
2022-05-08 16:36:58 +0200

    function to group df and agg

--- 03-Walmart_sales/src/data/data_processing.py
+++ 03-Walmart_sales/src/data/data_processing.py
@@ -40,3 +40,16 @@ def convert_date_fn(df,datecol):
            'dayofyear','dayofmonth','weekofyear', 'Weekly_Sales', 'Holiday_Flag', 'Temperature', 'Fuel_Price', 'CPI', 'Unemployment']]
     
     return X
+
+
+
+# Function to group input data frame and carry out aggregated sum or mean
+def group_df_agg_fn(df,group_col, col_eval, aggfn, div_by, div=True):
+       df_gp= df.copy()
+       df_gp=df_gp.groupby([group_col])[[col_eval]].agg(aggfn).sort_values(by=[col_eval],ascending=False).rename(columns={col_eval: aggfn + '_' + col_eval})
+       if True==div:
+              df_gp[aggfn + '_' + col_eval] = df_gp[aggfn + '_' + col_eval]/div_by
+              print('{} ({})'.format(aggfn + '_' + col_eval, "{:.2e}".format(div_by)))
+              return df_gp
+       else:
+              return df_gp
